f710aba833546c539d6c3be6e721ac4cddaf3ec5
KarolaRaid <karolaraid@gmail.com>
2024-09-15 13:49:29 +0300

    Add example.env file

--- koerailm_weather_app/settings.py
+++ koerailm_weather_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y6lnj=1x^j5wek0(7lf(6$jsr2$aa+_@@d+#xs!-_me@7(d&h0'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
