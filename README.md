3a2a735bdcb7e525a8588f5aa3b0585709ca38e3
AlvarLillemagi <lillemagi.alvar@gmail.com>
2024-09-16 16:54:47 +0300

    Changes made based on teacher's instructions last class

--- README.md
+++ README.md
@@ -11,3 +11,9 @@ Fun weather app with a dog weather index and Estonian folk sayings (SDA group fi
 3. Update requirements
 
     `pip freeze > requirements.txt`
+
+4. Setup database configuration in .env
+
+5. Apply migrations
+
+   `Python manage.py migrate`
--- koerailm_weather_app/settings.py
+++ koerailm_weather_app/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'koerailm_weather_app.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": config("DB_ENGINE"),
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOST"),
+        "PORT": config("DB_PORT"),
     }
 }
 
