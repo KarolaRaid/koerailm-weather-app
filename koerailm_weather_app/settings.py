ce3b229d69a617132823b7e810bdc46981525c9c
Raunc <reinkoolmann96@gmail.com>
2024-10-05 10:20:48 +0300

    Settings file update

--- koerailm_weather_app/settings.py
+++ koerailm_weather_app/settings.py
@@ -80,8 +80,8 @@ DATABASES = {
     "default": {
         "ENGINE": config("DB_ENGINE"),
         "NAME": config("DB_NAME"),
-        "USER": config("DB_USER", "postgres"),
-        "PASSWORD": config("DB_PASSWORD", "password"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
         "HOST": config("DB_HOST"),
         "PORT": config("DB_PORT"),
     }
