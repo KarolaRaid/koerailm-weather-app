6eafd126f13c31dc326f9acfcce011455a447af5
Raunc <reinkoolmann96@gmail.com>
2024-10-06 10:17:45 +0300

    Saving weather API

--- weather_data/views.py
+++ weather_data/views.py
@@ -2,6 +2,7 @@ import requests
 from django.shortcuts import render
 
 
+
 def index(request):
     # Placeholder for now. Eventually use user's real location (via IP or browser API) as default
     location = "Tallinn"
@@ -10,10 +11,35 @@ def index(request):
     response = requests.get(api_url)
 
     context = {}
+
     if response.status_code == 200:
         weather_data = response.json()
+
+
+        temp_c = weather_data.get('current', {}).get('temp_c', None)
+        condition_text = weather_data.get('current', {}).get('condition', {}).get('text', None)
+        wind_kph = weather_data.get('current', {}).get('wind_kph', None)
+        humidity = weather_data.get('current', {}).get('humidity', None)
+
+
+
+        # Save weather data to the database
+        if temp_c is not None and condition_text is not None:
+            weather_record = weather_data(
+                location_id=location,
+                temperature=temp_c,
+                humidity=humidity,
+                wind_speed=wind_kph,
+                condition=condition_text,
+
+            )
+            weather_record.save()
+
         context['weather_data'] = weather_data
-    context['weather_data'] = "No weather data available"
+    else:
+        context['weather_data'] = "No weather data available"
+
     return render(request, 'weather_data.html', context)
+
 
 
