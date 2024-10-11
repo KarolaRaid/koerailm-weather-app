b2bf9bf194b2839e7cac48a5a3b325e746a74ea3
KarolaRaid <karolaraid@gmail.com>
2024-10-11 12:02:29 +0300

    Add condition_text translation

--- weather_data/views.py
+++ weather_data/views.py
@@ -24,11 +24,12 @@ def current_weather(request):
     temperature_feelslike = weather_data.get('current').get('feelslike_c')
     icon = weather_data.get('current').get('condition').get('icon')
 
+
     # condition_text + wind_dir need to be translated (simple dictionary)
     context['location'] = location
     context['country'] = country
     context['temperature'] = temperature
-    context['condition_text'] = condition_text
+    context['condition_text'] = text_condition_trans(condition_text)
     context['wind'] = wind
     context['wind_direction'] = wind_direction
     context['precipitation'] = precipitation
@@ -43,3 +44,57 @@ def current_weather(request):
 #def location_search(request, location_input):
 #    return HttpResponseRedirect(f'/{location_input}/')
 
+
+def text_condition_trans(condition_text):
+    condition_text_ee = {
+        "Sunny": "Päikeseline",
+        "Clear": "Selge",
+        "Partly cloudy": "Kohati pilvine",
+        "Cloudy": "Pilvine",
+        "Overcast": "Pilvkate",
+        "Mist": "Kerge udu",
+        "Patchy rain possible": "Kohati võimalik vihm",
+        "Patchy snow possible": "Kohati võimalik lumi",
+        "Patchy sleet possible": "Kohati võimalik lörts",
+        "Patchy freezing drizzle possible": "Kohati võimalik jäätuv uduvihm",
+        "Thundery outbreaks possible": "Võimalikud äikesepuhangud",
+        "Blowing snow": "Tuisk",
+        "Blizzard": "Lumetorm",
+        "Fog": "Udu",
+        "Freezing fog": "Jäätuv udu",
+        "Patchy light drizzle": "Kohati nõrk uduvihm",
+        "Light drizzle": "Nõrk uduvihm",
+        "Freezing drizzle": "Jäätuv uduvihm",
+        "Heavy freezing drizzle": "Tugev jäätuv uduvihm",
+        "Patchy light rain": "Kohati nõrk vihm",
+        "Light rain": "Nõrk vihm",
+        "Moderate rain at times": "Vahelduvalt mõõdukas vihm",
+        "Moderate rain": "Mõõdukas vihm",
+        "Heavy rain at times": "Vahelduvalt tugev vihm",
+        "Heavy rain": "Tugev vihm",
+        "Light freezing rain": "Nõrk jäätuv vihm",
+        "Moderate or heavy freezing rain": "Mõõdukas või tugev jäätuv vihm",
+        "Light sleet": "Nõrk lörts",
+        "Moderate or heavy sleet": "Mõõdukas või tugev lörts",
+        "Patchy light snow": "Kohati nõrk lumi",
+        "Light snow": "Nõrk lumi",
+        "Patchy moderate snow": "Kohati mõõdukas lumi",
+        "Moderate snow": "Mõõdukas lumi",
+        "Patchy heavy snow": "Kohati tugev lumi",
+        "Heavy snow": "Tugev lumi",
+        "Ice pellets": "Rahe",
+        "Light rain shower": "Nõrk vihmahoog",
+        "Moderate or heavy rain shower": "Mõõdukas või tugev vihmahoog",
+        "Torrential rain shower": "Paduvihmahoog",
+        "Light sleet showers": "Nõrgad lörtsihood",
+        "Moderate or heavy sleet showers": "Mõõdukad või tugevad lörtsihood",
+        "Light snow showers": "Nõrgad lumesajud",
+        "Moderate or heavy snow showers": "Mõõdukad või tugevad lumesajud",
+        "Light showers of ice pellets": "Nõrgad rahesajud",
+        "Moderate or heavy showers of ice pellets": "Mõõdukad või tugevad rahesajud",
+        "Patchy light rain with thunder": "Kohati nõrk vihm äikesega",
+        "Moderate or heavy rain with thunder": "Mõõdukas või tugev vihm äikesega",
+        "Patchy light snow with thunder": "Kohati nõrk lumi äikesega",
+        "Moderate or heavy snow with thunder": "Mõõdukas või tugev lumi äikesega"
+    }
+    return condition_text_ee.get(condition_text)
