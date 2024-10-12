4b20457c9a56004e896077091777a22ef4e33bdf
KarolaRaid <karolaraid@gmail.com>
2024-10-12 23:41:20 +0300

    Change current weather API to forecast API

--- weather_data/api.py
+++ weather_data/api.py
@@ -4,15 +4,12 @@ from decouple import config
 
 def get_weather_data(location):
     is_estonia = False
-
     api_key = config('WEATHER_API_KEY')
-
     search_url = f'http://api.weatherapi.com/v1/search.json?key={api_key}&q={location}'
-    search_response = requests.get(search_url)
 
+    search_response = requests.get(search_url)
     if search_response.status_code == 200:
         search_results = search_response.json()
-
         if len(search_results) > 0:
             entry_preferred = None
             for result in search_results:
@@ -23,17 +20,15 @@ def get_weather_data(location):
                     break
 
             location = entry_preferred.get('name')
+            weather_url = f'http://api.weatherapi.com/v1/forecast.json?key={api_key}&q={location}&days=2'
 
-            weather_url = f'http://api.weatherapi.com/v1/current.json?key={api_key}&q={location}'
             if is_estonia:
                 weather_url += ',EE'
             weather_response = requests.get(weather_url)
             if weather_response.status_code == 200:
                 return weather_response.json()
-
         else:
             return {'error': 'Asukohta ei leitud. Palun proovige uuesti.'}
-
     return None
 
 
@@ -43,6 +38,7 @@ def get_weather_data_by_geolocation(latitude, longitude):
 
     api_key = config('WEATHER_API_KEY')
     url = f'http://api.weatherapi.com/v1/current.json?key={api_key}&q={lat},{lon}'
+
     response = requests.get(url)
     if response.status_code == 200:
         return response.json()
